384a028dc509a8911792814c570807c8ba7efe81
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-02-24 09:53:43 +0000

    Classes implemented. getTotalValue() with dummy value.

--- Lab2/Lab2_1StocksPortfolio/src/main/java/org/example/StocksPortfolio.java
+++ Lab2/Lab2_1StocksPortfolio/src/main/java/org/example/StocksPortfolio.java
@@ -1,4 +1,17 @@
 package org.example;
 
+import java.util.List;
+
 public class StocksPortfolio {
+    private List<Stock> stocks;
+    private IStockmarketService stockmarket;
+
+    public StocksPortfolio(IStockmarketService stockmarket) {
+        this.stockmarket = stockmarket;
+    }
+
+    public void addStock(Stock stock){};
+    public double getTotalValue(){
+        return 2.0;
+    }
 }
