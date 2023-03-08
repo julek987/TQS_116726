3961331bb5a8150cc74b50e3761c90c77ee16de1
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-03-08 16:08:19 +0000

    2.2 Geocoding initial commit.

---
+++ Lab2/Lab2_2Goecoding/src/main/java/org/example/Address.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Address {
+    private final String road;
+    private final String state;
+    private final String city;
+    private final String zip;
+    private final String houseNumber;
+
+    public Address(String road, String state, String city, String zip, String houseNumber) {
+        this.road = road;
+        this.state = state;
+        this.city = city;
+        this.zip = zip;
+        this.houseNumber = houseNumber;
+    }
+
+
+}
