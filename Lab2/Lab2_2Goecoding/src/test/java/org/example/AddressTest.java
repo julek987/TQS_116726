c676378d78ab0a2affa80b33cff6968acb97a406
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-03-08 16:39:13 +0000

    2.2 Tests added

---
+++ Lab2/Lab2_2Goecoding/src/test/java/org/example/AddressTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AddressTest {
+    Address address = new Address(
+            "890 Fifth Avenue",
+            "New York",
+            "NY",
+            "10021",
+            null);
+    String addressString = String.format(
+            "Address{road=%s,state=%s,city=%s,zip=%s,houseNumber=%s}",
+            "890 Fifth Avenue",
+            "New York",
+            "NY",
+            "10021",
+            null);
+
+    @Test
+    void testToString() {
+        Assertions.assertEquals(address.toString(), addressString);
+    }
+
+    @Test
+    void testHashCode() {
+        Assertions.assertEquals(address.hashCode(), 101);
+    }
+    @Test
+    void testEquals() {
+        Address testAddress = new Address(
+                "890 Fifth Avenue",
+                "New York",
+                "NY",
+                "10021",
+                null);
+
+        Assertions.assertEquals(address, testAddress);
+    }
+}
