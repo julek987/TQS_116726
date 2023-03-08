c92ab7e7df27de9908cd94d16e662f6e0d47d519
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-03-08 16:41:02 +0000

    2.2 AddressResolverTest skeleton copied from given source.

--- Lab2/Lab2_2Goecoding/src/test/java/org/example/AddressResolverTest.java
+++ Lab2/Lab2_2Goecoding/src/test/java/org/example/AddressResolverTest.java
@@ -1,4 +1,54 @@
 package org.example;
 
-public class AddressResolverTest {
+import org.json.simple.parser.ParseException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+
+@ExtendWith(MockitoExtension.class)
+class AddressResolverTest {
+
+    AddressResolver resolver;
+
+    @Test
+    @Disabled
+    void whenResolveDetiGps_returnJacintoMagalhaeAddress() throws ParseException, IOException, URISyntaxException {
+
+        //todo: implement test; remove Disabled annotation
+
+        // will crash for now...need to set the resolver before using it
+        Optional<Address> result = resolver.findAddressForLocation( 40.633116,-8.658784);
+
+        //return
+        Address expected = new Address( "Avenida JoĂŁo Jacinto de MagalhĂŁes", "Aveiro", "", "3810-149", null);
+
+        assertTrue( result.isPresent());
+        assertEquals( expected, result.get());
+
+    }
+
+    @Test
+    @Disabled
+    public void whenBadCoordidates_thenReturnNoValidAddress() throws IOException, URISyntaxException, ParseException {
+
+        ///todo: implement test
+
+
+        Optional<Address> result = resolver.findAddressForLocation( -361,-361);
+        // verify no valid result
+        assertFalse( result.isPresent());
+
+    }
 }
