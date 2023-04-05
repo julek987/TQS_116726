608e4e72153009947872e22b7e33aeed86cdc6fa
Juliusz Szymajda <234769@edu.p.lodz.pl>
2023-04-05 17:02:39 +0100

    HW initial commit

---
+++ HW_116726/src/main/java/com/example/hw_116726/Hw116726Application.java
@@ -0,0 +1,13 @@
+package com.example.hw_116726;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw116726Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw116726Application.class, args);
+	}
+
+}
